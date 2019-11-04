80426ad5609cab0c961e9d8993ca4d5585fc915b
장윤진 <yunjin_jang@wemakeprice.com>
2019-11-04 23:01:14 +0900

    테스트 자동화를 위한 일관성 있는 코드로 개선

--- src/main/java/com/yun27jin/toby/user/dao/UserDao.java
+++ src/main/java/com/yun27jin/toby/user/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.yun27jin.toby.user.dao;
 
 import com.yun27jin.toby.user.domain.User;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import javax.sql.DataSource;
 import java.sql.*;
@@ -20,29 +21,54 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException {
-        Connection c = this.dataSource.getConnection();
-        PreparedStatement psmt = c.prepareStatement("INSERT INTO toby.users(id, password, name) VALUES (?, ?, ?)");
-        psmt.setString(1, user.getId());
-        psmt.setString(2, user.getPassword());
-        psmt.setString(3, user.getName());
-        psmt.executeUpdate();
-        psmt.close();
-        c.close();
+        Connection connection = this.dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO toby.users(id, password, name) VALUES (?, ?, ?)");
+        preparedStatement.setString(1, user.getId());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.setString(3, user.getName());
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+        connection.close();
     }
 
     public User get(String id) throws SQLException {
-        Connection c = this.dataSource.getConnection();
-        PreparedStatement psmt = c.prepareStatement("SELECT * FROM toby.users");
-        ResultSet rs = psmt.executeQuery();
-        rs.next();
-        User user = new User();
-        user.setId(rs.getString("id"));
-        user.setPassword(rs.getString("password"));
-        user.setName(rs.getString("name"));
-        rs.close();
-        psmt.close();
-        c.close();
+        Connection connection = this.dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM toby.users WHERE id = ?");
+        preparedStatement.setString(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        User user = null;
+        if (resultSet.next()) {
+            user = new User();
+            user.setId(resultSet.getString("id"));
+            user.setPassword(resultSet.getString("password"));
+            user.setName(resultSet.getString("name"));
+        }
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        if (user == null)
+            throw new EmptyResultDataAccessException(1);
         return user;
+    }
+
+    public void deleteAll() throws SQLException {
+        Connection c = this.dataSource.getConnection();
+        PreparedStatement preparedStatement = c.prepareStatement("DELETE FROM toby.users");
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+        c.close();
+    }
+
+    public int getCount() throws SQLException {
+        Connection c = this.dataSource.getConnection();
+        PreparedStatement preparedStatement = c.prepareStatement("SELECT count(*) FROM toby.users");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        int count = resultSet.getInt(1);
+        resultSet.close();
+        preparedStatement.close();
+        c.close();
+        return count;
     }
 
 }
---
+++ src/test/java/com/yun27jin/toby/test/Main.java
@@ -0,0 +1,9 @@
+package com.yun27jin.toby.test;
+
+import org.junit.runner.JUnitCore;
+
+public class Main {
+    public static void main(String[] args) {
+        JUnitCore.main("com.yun27jin.toby.test.UserDaoTest");
+    }
+}
